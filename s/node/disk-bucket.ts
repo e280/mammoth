586bd3ecdd5e9a03dc652717dd2fde80cbdfc17a
Chase Moskal <chasemoskal@gmail.com>
2026-07-17 21:33:46 -0700

    add: node disk bucket

---
+++ s/node/disk-bucket.ts
@@ -0,0 +1,60 @@
+
+import {join} from "node:path"
+import {Readable} from "node:stream"
+import {pipeline} from "node:stream/promises"
+import {mkdir, stat, unlink} from "node:fs/promises"
+import {createWriteStream, openAsBlob} from "node:fs"
+
+import {Bucket, Id} from "../core/types.js"
+import {isNotFound} from "./utils/is-not-found.js"
+
+export class DiskBucket implements Bucket {
+	#directory
+
+	constructor(directory: string) {
+		this.#directory = directory
+	}
+
+	async has(id: Id) {
+		try {
+			await stat(this.#path(id))
+			return true
+		}
+		catch (error) {
+			if (isNotFound(error)) return false
+			throw error
+		}
+	}
+
+	async size(id: Id) {
+		const stats = await stat(this.#path(id))
+		return stats.size
+	}
+
+	async delete(id: Id) {
+		try {
+			await unlink(this.#path(id))
+		}
+		catch (error) {
+			if (!isNotFound(error))
+				throw error
+		}
+	}
+
+	async read(id: Id) {
+		return openAsBlob(this.#path(id))
+	}
+
+	async write(id: Id, readable: ReadableStream<Uint8Array>) {
+		await mkdir(this.#directory, {recursive: true})
+		await pipeline(
+			Readable.fromWeb(readable as any),
+			createWriteStream(this.#path(id)),
+		)
+	}
+
+	#path(id: Id) {
+		return join(this.#directory, id)
+	}
+}
+
---
+++ s/node/utils/is-not-found.ts
@@ -0,0 +1,9 @@
+
+export function isNotFound(error: unknown): error is NodeJS.ErrnoException {
+	return (
+		error instanceof Error
+		&& "code" in error
+		&& error.code === "ENOENT"
+	)
+}
+
